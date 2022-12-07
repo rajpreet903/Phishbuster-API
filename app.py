5d2fb5729c65a80f315acfb246771ecbf43bb352
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2022-12-07 15:42:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ def predict(data: UrlData):
     return prediction
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
     
     # changed 127.0.0.1 to 0.0.0.0 for railway.app deployment
 
